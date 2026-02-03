4b31a6230e7c3663a387fd9dae0fa25f10b94186
Anyelo Reyes Guevara <aerguevara@gmail.com>
2026-02-03 19:35:04 +0100

    Initial Commit

---
+++ anyelito/anyelitoApp.swift
@@ -0,0 +1,17 @@
+//
+//  anyelitoApp.swift
+//  anyelito
+//
+//  Created by Anyelo Reyes on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct anyelitoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ anyelitoTests/anyelitoTests.swift
@@ -0,0 +1,17 @@
+//
+//  anyelitoTests.swift
+//  anyelitoTests
+//
+//  Created by Anyelo Reyes on 3/2/26.
+//
+
+import Testing
+@testable import anyelito
+
+struct anyelitoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
